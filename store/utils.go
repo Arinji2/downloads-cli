24023953936357683f72f213f5221de0f4ff5d6b
Arinji2 <arinjaydhar205@gmail.com>
2025-01-15 20:27:55 +0530

    Add a helper to go to mod dir

--- store/utils.go
+++ store/utils.go
@@ -3,11 +3,37 @@ package store
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 
 	"github.com/Arinji2/downloads-cli/logger"
 )
+
+func changeToGoModDir() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current directory: %v", err)
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			if err := os.Chdir(dir); err != nil {
+				logger.GLogger.AddToLog("FATAL", err.Error())
+				os.Exit(1)
+			}
+			return
+		}
+
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			logger.GLogger.AddToLog("FATAL", "Could not find go.mod file")
+			os.Exit(1)
+		}
+		dir = parentDir
+	}
+}
 
 func readAndParseStoredData(s *Store) ([]StoredData, error) {
 	if !s.cacheExpired {
