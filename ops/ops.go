e7b48dfae199c997bf793bc1ae8d1ff74482763f
Arinji2 <arinjaydhar205@gmail.com>
2025-01-17 00:33:59 +0530

    Add Testing Init Function

--- ops/ops.go
+++ ops/ops.go
@@ -3,13 +3,23 @@ package ops
 import "github.com/Arinji2/downloads-cli/store"
 
 type Operation struct {
-	Name  string
-	Store *store.Store
+	Name      string
+	Store     *store.Store
+	IsTesting bool
 }
 
 func InitOperations(name string, s *store.Store) *Operation {
 	return &Operation{
-		Name:  name,
-		Store: s,
+		Name:      name,
+		Store:     s,
+		IsTesting: false,
+	}
+}
+
+func InitTestingOperations(name string, s *store.Store) *Operation {
+	return &Operation{
+		Name:      name,
+		Store:     s,
+		IsTesting: true,
 	}
 }
