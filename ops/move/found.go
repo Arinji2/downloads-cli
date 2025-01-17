409227824f74da98e8d9c0b4c30eafadc7843b08
Arinji2 <arinjaydhar205@gmail.com>
2025-01-17 11:47:17 +0530

    Finish Found Function for Custom Move

--- ops/move/found.go
+++ ops/move/found.go
@@ -40,3 +40,32 @@ func FoundDefaultMove(data store.StoredData, m *Move) (err error) {
 	}
 	return nil
 }
+
+func FoundCustomMove(data store.StoredData, m *Move) (err error) {
+	data.InProgress = true
+	m.Operations.Store.UpdateStoredData(data.ID, data)
+	rawMoveType, err := utils.GetOperationType(data.Args[0])
+	if err != nil {
+		return err
+	}
+	moveType := MoveType(rawMoveType)
+	if moveType != MoveMC {
+		return fmt.Errorf("invalid move type")
+	}
+	fileName := data.Args[0]
+	originalPath := data.Args[1]
+	destPath := data.Args[2]
+	if !strings.HasSuffix(destPath, fileName) {
+		destPath = fmt.Sprintf("%s/%s", destPath, fileName)
+	}
+
+	m.Operations.Store.DeleteStoredData(data.ID)
+	if m.Operations.IsTesting {
+		return nil
+	}
+	err = os.Rename(originalPath, destPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
