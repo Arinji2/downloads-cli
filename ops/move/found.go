312e64a8f8ced9a95cf9fd4e7b055ac40a3400d9
Arinji2 <arinjaydhar205@gmail.com>
2025-01-17 11:47:04 +0530

    Finish Found Function for Default Move

---
+++ ops/move/found.go
@@ -0,0 +1,42 @@
+package move
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Arinji2/downloads-cli/store"
+	"github.com/Arinji2/downloads-cli/utils"
+)
+
+func FoundDefaultMove(data store.StoredData, m *Move) (err error) {
+	data.InProgress = true
+	m.Operations.Store.UpdateStoredData(data.ID, data)
+	rawMoveType, err := utils.GetOperationType(data.Args[0])
+	if err != nil {
+		return err
+	}
+	moveType := MoveType(rawMoveType)
+	if moveType != MoveMD {
+		return fmt.Errorf("invalid move type")
+	}
+	fileName := data.Args[0]
+	originalPath := data.Args[1]
+	destPath := m.MovePresets[data.Args[2]]
+	if destPath == "" {
+		return fmt.Errorf("invalid move string for move default")
+	}
+	if !strings.HasSuffix(destPath, fileName) {
+		destPath = fmt.Sprintf("%s/%s", destPath, fileName)
+	}
+
+	m.Operations.Store.DeleteStoredData(data.ID)
+	if m.Operations.IsTesting {
+		return nil
+	}
+	err = os.Rename(originalPath, destPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
