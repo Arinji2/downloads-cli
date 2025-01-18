bca4d2cff4e7bab41d3aa8761d16fb68e126d1b4
Arinji2 <arinjaydhar205@gmail.com>
2025-01-18 20:07:17 +0530

    4. Probable Fix

--- ops/move/found.go
+++ ops/move/found.go
@@ -66,9 +66,17 @@ func FoundCustomMove(data store.StoredData, m *Move) (moved bool, err error) {
 	fmt.Println("3] OriginalPath", originalPath)
 
 	if runtime.GOOS == "windows" {
-		firstIndex := (strings.Index(destPath, ":")) + 1
-		beforeMount := destPath[:firstIndex]
-		afterMount := destPath[firstIndex:]
+		firstIndex := (strings.Index(originalPath, ":")) + 1
+		beforeMount := originalPath[:firstIndex]
+		afterMount := originalPath[firstIndex:]
+		println("1] BeforeMount", beforeMount)
+		println("1] AfterMount", afterMount)
+		afterMount = strings.ReplaceAll(afterMount, ":", "_")
+		originalPath = beforeMount + afterMount
+
+		firstIndex = (strings.Index(destPath, ":")) + 1
+		beforeMount = destPath[:firstIndex]
+		afterMount = destPath[firstIndex:]
 		println("1] BeforeMount", beforeMount)
 		println("1] AfterMount", afterMount)
 		afterMount = strings.ReplaceAll(afterMount, ":", "_")
