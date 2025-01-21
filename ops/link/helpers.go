1d1465ee441281d2ce2b670b1793a5c85e45cfe4
Arinji2 <arinjaydhar205@gmail.com>
2025-01-21 16:00:41 +0530

    Finish Watcher Setup for Links

---
+++ ops/link/helpers.go
@@ -0,0 +1,35 @@
+package link
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/Arinji2/downloads-cli/logger"
+	"github.com/Arinji2/downloads-cli/utils"
+)
+
+func verifyLink(fileName string) (linkType LinkType, err error) {
+	parts := strings.Split(fileName, ".")
+	if len(parts) < 2 {
+		logger.GlobalLogger.AddToLog("ERROR", "invalid file name for link")
+		return "", fmt.Errorf("invalid file name for link")
+	}
+
+	nameParts := strings.Split(parts[0], "-")
+	if len(nameParts) < 3 {
+		return "", fmt.Errorf("invalid file name for link")
+	}
+
+	_, err = utils.GetOperationType(fileName)
+	if err != nil {
+		return "", err
+	}
+	linkStr := nameParts[1]
+	if linkStr == "t" || linkStr == "p" {
+		linkType = LinkType(linkStr)
+	} else {
+		return "", errors.New("link type not valid")
+	}
+	return linkType, nil
+}
---
+++ ops/link/link.go
@@ -0,0 +1,86 @@
+package link
+
+import (
+	"time"
+
+	"github.com/Arinji2/downloads-cli/logger"
+	"github.com/Arinji2/downloads-cli/ops"
+	"github.com/Arinji2/downloads-cli/store"
+)
+
+var DEFAULT_LINK_INTERVAL = 5
+
+type LinkType string
+
+const (
+	LinkTemp LinkType = "t"
+	LinkPerm LinkType = "p"
+)
+
+type Link struct {
+	Operations    *ops.Operation
+	CheckInterval int
+}
+
+func InitLink(o *ops.Operation, interval int) *Link {
+	if interval == 0 {
+		interval = DEFAULT_LINK_INTERVAL
+	}
+	return &Link{
+		Operations:    o,
+		CheckInterval: interval,
+	}
+}
+
+func (l *Link) NewLinkRegistered(fileName string, pathName string) error {
+	linkType, err := verifyLink(fileName)
+	if err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	id, err := store.GenerateStoreID(l.Operations.Store)
+	if err != nil {
+		return err
+	}
+	storeFile := store.StoredData{
+		ID:   id,
+		Task: "LINK",
+		Args: []string{
+			fileName,
+			string(linkType),
+			pathName,
+		},
+		InProgress: false,
+	}
+
+	l.Operations.Store.AddStoredData(storeFile)
+
+	return nil
+}
+
+func (l *Link) RunLinkJobs() {
+	ticker := time.NewTicker(time.Second * time.Duration(l.CheckInterval))
+	for range ticker.C {
+
+		storedData, err := l.Operations.Store.GetAllStoredData()
+		if err != nil {
+			logger.GlobalLogger.AddToLog("ERROR", err.Error())
+			break
+		}
+		for _, data := range storedData {
+			if data.Task == "LINK" {
+				if data.InProgress {
+					continue
+				}
+				_, err := FoundDelete(data, l)
+				if err != nil {
+					logger.GlobalLogger.AddToLog("ERROR", err.Error())
+					continue
+				}
+
+			}
+		}
+	}
+}
