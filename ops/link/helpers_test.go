d65dcc3e35bc78ffc746575b55aaa4986e2a286e
Arinji2 <arinjaydhar205@gmail.com>
2025-01-22 18:38:08 +0530

    Add testing for Link Init

---
+++ ops/link/helpers_test.go
@@ -0,0 +1,53 @@
+package link_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/Arinji2/downloads-cli/logger"
+	"github.com/Arinji2/downloads-cli/ops"
+	"github.com/Arinji2/downloads-cli/ops/link"
+	"github.com/Arinji2/downloads-cli/store"
+)
+
+// setupFS creates necessary test files and directories
+func setupFS(t *testing.T, tempDir, name string, typeOfLink link.LinkType) (fileName, testFile, destPath string) {
+	t.Helper()
+
+	destPath = filepath.Join(tempDir, "test")
+	if err := os.MkdirAll(destPath, 0755); err != nil {
+		t.Fatalf("Failed to create test directory: %v", err)
+	}
+
+	fileName = fmt.Sprintf("l-%s-%s.txt", typeOfLink, name)
+	testFile = filepath.Join(tempDir, fileName)
+	if err := os.WriteFile(testFile, []byte("test"), 0644); err != nil {
+		t.Fatalf("Failed to create test file: %v", err)
+	}
+
+	return fileName, testFile, destPath
+}
+
+// setupTest initializes test environment with store and operations
+func setupTest(t *testing.T) (*store.Store, string, *ops.Operation) {
+	log, err := logger.NewLogger("log.txt", 1024*1024, "TEST")
+	if err != nil {
+		t.Fatalf("Failed to create logger: %v", err)
+	}
+	logger.GlobalizeLogger(log)
+
+	t.Helper()
+
+	tempDir := t.TempDir()
+	storeFile := filepath.Join(tempDir, "test-store.json")
+
+	s := store.NewStore(storeFile)
+	if err := s.Reset(); err != nil {
+		t.Fatalf("Failed to init store: %v", err)
+	}
+
+	ops := ops.InitTestingOperations("LINK", s)
+	return s, tempDir, ops
+}
---
+++ ops/link/link_test.go
@@ -0,0 +1,26 @@
+package link_test
+
+import (
+	"testing"
+
+	"github.com/Arinji2/downloads-cli/ops/link"
+)
+
+func TestNewLinkRegistered(t *testing.T) {
+	s, tempDir, ops := setupTest(t)
+
+	fileName, testFile, _ := setupFS(t, tempDir, "test", link.LinkType("t"))
+	linkJob := link.InitLink(ops, 0)
+
+	if err := linkJob.NewLinkRegistered(fileName, testFile); err != nil {
+		t.Fatalf("Failed to register new link: %v", err)
+	}
+
+	storedData, err := s.GetAllStoredData()
+	if err != nil {
+		t.Error(err)
+	}
+	if len(storedData) != 1 {
+		t.Error("Expected 1 stored data, got ", len(storedData))
+	}
+}
