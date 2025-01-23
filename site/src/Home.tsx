3cbcdc8c25198ca906ba5bbea7ffebe6d284caea
Arinji2 <arinjaydhar205@gmail.com>
2025-01-23 19:50:27 +0530

    Finish UI

--- site/src/Home.tsx
+++ site/src/Home.tsx
@@ -2,12 +2,9 @@ import { useEffect } from "react";
 
 const URL_REGEX = /^[a-zA-Z0-9]+\.[a-zA-Z]+$/;
 export default function Home() {
-  useEffect(() => {
-    const hash = window.location.hash.slice(1);
-    const params = new URLSearchParams(hash);
-
-    const urlID = params.get("urlID");
-    const type = params.get("type");
+  const redirectFunc = () => {
+    const urlID = window.location.hash.slice(1);
+    const type = window.location.hash.slice(1);
     if (urlID && type) {
       if (URL_REGEX.test(urlID)) {
         if (type === "t") {
@@ -17,6 +14,24 @@ export default function Home() {
         }
       }
     }
+  };
+  useEffect(() => {
+    redirectFunc();
   }, []);
-  return <div className="w-full h-full"></div>;
+  return (
+    <div className="py-4 w-full h-[100svh] gap-6 bg-slate-800 flex flex-col items-center justify-center">
+      <h1 className="font-bold md:text-6xl text-4xl text-white text-center">
+        "Downloads On Steroids"
+      </h1>
+      <p className="text-white/50 text-xl md:text-2xl">
+        You will be redirected shortly.
+      </p>
+      <button
+        onClick={redirectFunc}
+        className=" text-white/50 shadow-black shadow-md mt-20 text-center hover:shadow-sm transition-all ease-in-out duration-300 px-6 py-4 text-sm md:text-base p-2 rounded-md"
+      >
+        If you haven't been redirected yet, click here.
+      </button>
+    </div>
+  );
 }
