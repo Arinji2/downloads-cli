90c4aed8ab4d56dad45e84f2f7ae3f73050482fd
Arinji2 <arinjaydhar205@gmail.com>
2025-01-23 19:42:27 +0530

    Add in File Router

--- site/src/Home.tsx
+++ site/src/Home.tsx
@@ -1,3 +1,22 @@
+import { useEffect } from "react";
+
+const URL_REGEX = /^[a-zA-Z0-9]+\.[a-zA-Z]+$/;
 export default function Home() {
+  useEffect(() => {
+    const hash = window.location.hash.slice(1);
+    const params = new URLSearchParams(hash);
+
+    const urlID = params.get("urlID");
+    const type = params.get("type");
+    if (urlID && type) {
+      if (URL_REGEX.test(urlID)) {
+        if (type === "t") {
+          window.location.href = `https://litter.catbox.moe/${urlID}`;
+        } else if (type === "p") {
+          window.location.href = `https://files.catbox.moe/${urlID}`;
+        }
+      }
+    }
+  }, []);
   return <div className="w-full h-full"></div>;
 }
