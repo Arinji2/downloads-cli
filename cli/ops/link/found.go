ef3a685674dc31d683ba5df7f4b839d02a73b25f
Arinji2 <arinjaydhar205@gmail.com>
2025-01-28 00:27:26 +0530

    Use core packages for link

--- cli/ops/link/found.go
+++ cli/ops/link/found.go
@@ -6,12 +6,13 @@ import (
 	"path/filepath"
 
 	"github.com/Arinji2/downloads-cli/logger"
+	"github.com/Arinji2/downloads-cli/ops/core"
 	"github.com/Arinji2/downloads-cli/store"
 )
 
 func FoundLink(data store.StoredData, l *Link) (bool, string, error) {
-	data.InProgress = true
-	l.Operations.Store.UpdateStoredData(data.ID, data)
+	core.UpdateProgress(data, true, l.Operations)
+	defer l.Operations.Store.DeleteStoredData(data.ID)
 
 	if l.Operations.IsTesting {
 		return true, "", nil
@@ -32,7 +33,6 @@ func FoundLink(data store.StoredData, l *Link) (bool, string, error) {
 	if err != nil {
 		return false, "", err
 	}
-	l.Operations.Store.DeleteStoredData(data.ID)
 	logger.GlobalLogger.AddToLog("INFO", fmt.Sprintf("Created link for file: %s", data.Args[0]))
 
 	return true, d, nil
