bd29f614b7a85d55dee5e8eaa88db16e3d4ec2fb
Arinji2 <arinjaydhar205@gmail.com>
2025-01-29 00:36:46 +0530

    Make Rename to Link a core function

--- cli/ops/core/fs.go
+++ cli/ops/core/fs.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -34,4 +35,17 @@ func RenameToFilename(destPath string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+func RenameToLink(urlID string, typeOfLink string, path string) (string, bool, error) {
+	fileName := filepath.Base(path)
+	url := fmt.Sprintf("dos.arinji.com#urlID=%s&type=%s", urlID, typeOfLink)
+
+	newPath := strings.ReplaceAll(path, fileName, url)
+	err := os.Rename(path, newPath)
+	if err != nil {
+		return "", false, err
+	}
+
+	return newPath, true, nil
 }
