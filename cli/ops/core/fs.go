15c8abcb24fdaa7497cbd8fe77c98e01fd8f7bf7
Arinji2 <arinjaydhar205@gmail.com>
2025-01-28 20:14:22 +0530

    Add back windows fix

--- cli/ops/core/fs.go
+++ cli/ops/core/fs.go
@@ -3,12 +3,17 @@ package core
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
 func MoveFile(originalPath, destPath, fileName string) (bool, string, error) {
 	if !strings.HasSuffix(destPath, fileName) {
 		destPath = filepath.Join(destPath, fileName)
+	}
+	if runtime.GOOS == "windows" {
+		originalPath = WindowsMountIssue(originalPath)
+		destPath = WindowsMountIssue(destPath)
 	}
 
 	err := os.Rename(originalPath, destPath)
