f872d231e1c04d74ef04d91ea2631f09362a3d10
Arinji2 <arinjaydhar205@gmail.com>
2025-01-29 16:31:04 +0530

    Add Logo + Brand Colors

--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -6,6 +6,22 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          background: "#161616",
+          primary: "#2C9E05",
+          offWhite: "#C3C3C3",
+          darkWhite: "#656363",
+          darkBlue: "#448293",
+          lightBlue: "#1F74CF",
+          darkYellow: "#D5B169",
+          lightYellow: "#CEAF10",
+          grey: "#929292",
+        },
+      },
+    },
+  },
   plugins: [],
 } satisfies Config;
