b1c874afc653406e92c6134007fcb99037dea4ff
Arinji2 <arinjaydhar205@gmail.com>
2025-02-02 21:23:54 +0530

    Add Icons

---
+++ site/src/icons/copy.tsx
@@ -0,0 +1,7 @@
+import { IconProps, IconSvg } from "./base";
+
+export const CopyIcon = (props: IconProps) => (
+  <IconSvg viewBox="0 0 32 32" fill="none" stroke="currentColor" {...props}>
+    <path fill="currentColor" d="M4 2h11v2H6v13H4zm4 4h12v16H8zm2 2v12h8V8z" />
+  </IconSvg>
+);
---
+++ site/src/icons/laptop.tsx
@@ -0,0 +1,11 @@
+import { IconProps, IconSvg } from "./base";
+
+export const DeviceLaptopIcon = (props: IconProps) => (
+  <IconSvg viewBox="0 0 32 32" fill="none" stroke="currentColor" {...props}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 4H4v12h16V4zm12 2v8H6V6zm4 12H2v2h20z"
+    />
+  </IconSvg>
+);
