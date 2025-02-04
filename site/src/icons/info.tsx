88c6ac38211d991f32f4993cea6ee8759861a495
Arinji2 <arinjaydhar205@gmail.com>
2025-02-04 23:30:34 +0530

    Update Icons

--- site/src/icons/info.tsx
+++ site/src/icons/info.tsx
@@ -1,10 +1,9 @@
 import { IconProps, IconSvg } from "./base";
 
 export const InfoBoxIcon = (props: IconProps) => (
-  <IconSvg viewBox="0 0 32 32" fill="none" stroke="currentColor" {...props}>
+  <IconSvg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
     <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       d="M3 3h2v18H3zm16 0H5v2h14v14H5v2h16V3zm-8 6h2V7h-2zm2 8h-2v-6h2z"
     />
   </IconSvg>
