24d9d0d68c3d900499a26dbe0627b3d7d3582cd2
Arinji2 <arinjaydhar205@gmail.com>
2025-02-05 08:10:12 +0530

    Add New Icons and update packages

---
+++ site/src/icons/check.tsx
@@ -0,0 +1,10 @@
+import { IconProps, IconSvg } from "./base";
+
+export const CheckIcon = (props: IconProps) => (
+  <IconSvg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
+    <path
+      fill="currentColor"
+      d="M5 3H3v18h18V3zm0 2h14v14H5zm4 7H7v2h2v2h2v-2h2v-2h2v-2h2V8h-2v2h-2v2h-2v2H9z"
+    />
+  </IconSvg>
+);
