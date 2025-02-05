999af817a345809cc272c8700435d2584d8a2a02
Arinji2 <arinjaydhar205@gmail.com>
2025-02-05 08:32:33 +0530

    Add some animations for copy indicator

--- site/src/components/example-item.client.tsx
+++ site/src/components/example-item.client.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { CheckIcon } from "@/icons/check";
 import { CopyIcon } from "@/icons/copy";
 import { InfoBoxIcon } from "@/icons/info";
 import Link from "next/link";
+import { useState } from "react";
+import { cn } from "../../utils/cn";
 
 export function Item({
   index,
@@ -17,36 +20,58 @@ export function Item({
   infoLink: string;
   children: React.ReactNode;
 }) {
+  const [showCheck, setShowCheck] = useState(false);
   return (
-    <div className="w-fit h-fit flex flex-col gap-4">
-      <h4 className="font-bold text-lg tracking-tighter text-white">
+    <div className="flex h-fit w-fit flex-col gap-4">
+      <h4 className="text-lg font-bold tracking-tighter text-white">
         <span className="text-brand-primaryLight">{index})</span> {name}
       </h4>
       {children}
-      <div className="mt-10 relative px-6 shadow-brand py-2 bg-[#323232] w-fit h-fit flex flex-row items-center justify-start gap-2">
-        <div className="h-10 w-fit flex flex-row items-center justify-center px-2 gap-2 absolute -top-full right-0 bg-[#323232]">
+      <div className="relative mt-10 flex h-fit w-fit flex-row items-center justify-start gap-2 bg-[#323232] px-6 py-2 shadow-brand">
+        <div className="absolute -top-full right-0 flex h-10 w-fit flex-row items-center justify-center gap-2 bg-[#323232] px-2">
           <Link href={infoLink}>
             <InfoBoxIcon
               strokeWidth={0.5}
-              className="size-5  text-brand-darkYellow"
+              className="size-5 text-brand-darkYellow"
             />
           </Link>
 
           <button
             onClick={() => {
               navigator.clipboard.writeText(args.join("-"));
+              setShowCheck(true);
+              setTimeout(() => {
+                setShowCheck(false);
+              }, 1000);
             }}
+            className="relative flex flex-col items-center justify-center size-5 overflow-hidden"
           >
+            <CheckIcon
+              className={cn(
+                "duration-2 absolute size=5 text-brand-primaryLight transition-all ease-in-out",
+                {
+                  "translate-x-full opacity-0": !showCheck,
+                },
+              )}
+              aria-disabled={showCheck}
+              strokeWidth={0.5}
+            />
             <CopyIcon
               strokeWidth={0.5}
-              className="size-5  text-brand-darkBlue"
+              className={cn(
+                "size-5 absolute  text-brand-darkBlue transition-all duration-200 ease-in-out",
+                {
+                  "-translate-x-full opacity-0": showCheck,
+                },
+              )}
+              aria-disabled={!showCheck}
             />
           </button>
         </div>
         {args.map((item, index) => {
           return (
             <div
-              className="flex flex-row items-center justify-center w-fit h-fit gap-4"
+              className="flex h-fit w-fit flex-row items-center justify-center gap-4"
               key={index}
             >
               <p className="text-sm text-brand-offWhite">{item}</p>
