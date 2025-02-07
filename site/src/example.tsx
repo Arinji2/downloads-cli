1072426d540dbc8568864888d2baa419c526fb5a
Arinji2 <arinjaydhar205@gmail.com>
2025-02-07 23:45:56 +0530

    Map Demo Links to ConventionsData

--- site/src/example.tsx
+++ site/src/example.tsx
@@ -8,37 +8,47 @@ export const ConventionsData = [
         args: ["md", "pictures", "test.png"],
         infoLink: "/move?selected=default",
         description: `<span class="text-sm text-brand-offWhite">
-  Move a file, using a location preset in your 
-  <span class="text-brand-primaryLight">options file</span>
-</span>`,
+          Move a file, using a location preset in your 
+          <span class="text-brand-primaryLight">options file</span>
+        </span>`,
+        demo: {
+          terminal: "https://cdn.arinji.com/u/P2qlH9.mp4",
+          explorer: "https://cdn.arinji.com/u/U8mnff.mp4",
+        },
       },
       {
         name: "Move Custom",
         args: ["mc", "~#pictures#test", "test.png"],
         infoLink: "/move?selected=custom",
         description: `<span class="text-sm text-brand-offWhite"> 
-  Move a file, using a custom location 
-  <span class="text-brand-primaryLight">
-    using # as seperators
-  </span>
-</span>`,
+          Move a file, using a custom location 
+          <span class="text-brand-primaryLight">
+            using # as separators
+          </span>
+        </span>`,
+        demo: {
+          terminal: "https://cdn.arinji.com/u/JuQIMk.mp4",
+          explorer: "https://cdn.arinji.com/u/uBfSEp.mp4",
+        },
       },
-
       {
         name: "Move Custom Default",
         args: ["mcd", "pictures#test", "test.png"],
         infoLink: "/move?selected=customdefault",
         description: `<span class="text-sm text-brand-offWhite">
-  Move a file, using a custom location 
-  <span class="text-brand-primaryLight">
-    using # as seperators
-  </span> based on a default location preset in your 
-  <span class="text-brand-primaryLight">options file</span>
-      </span>`,
+          Move a file, using a custom location 
+          <span class="text-brand-primaryLight">
+            using # as separators
+          </span> based on a default location preset in your 
+          <span class="text-brand-primaryLight">options file</span>
+        </span>`,
+        demo: {
+          terminal: "https://cdn.arinji.com/u/i9m9RK.mp4",
+          explorer: "https://cdn.arinji.com/u/xSjmvj.mp4",
+        },
       },
     ],
   },
-
   {
     name: "LINK",
     description: `Conventions to help you convert your downloads, into shareable links.`,
@@ -48,16 +58,24 @@ export const ConventionsData = [
         args: ["l", "t", "test.png"],
         infoLink: "/link?selected=temporary",
         description: `<span class="text-sm text-brand-offWhite">
-   Make a temporary cdn link for a file, upto 150mb which will expire in a hour.
-  </span>`,
+          Make a temporary CDN link for a file, up to 150MB which will expire in an hour.
+        </span>`,
+        demo: {
+          terminal: "https://cdn.arinji.com/u/ZAq5g9.mp4",
+          explorer: "https://cdn.arinji.com/u/OOtbFf.mp4",
+        },
       },
       {
         name: "Link Permanent",
         args: ["l", "p", "test.png"],
         infoLink: "/link?selected=permanent",
         description: `<span class="text-sm text-brand-offWhite">
-   Make a permanent cdn link for a file, upto 100mb which will never expire.
-  </span>`,
+          Make a permanent CDN link for a file, up to 100MB which will never expire.
+        </span>`,
+        demo: {
+          terminal: "https://cdn.arinji.com/u/RK9q17.mp4",
+          explorer: "https://cdn.arinji.com/u/sLYhRJ.mp4",
+        },
       },
     ],
   },
@@ -70,8 +88,12 @@ export const ConventionsData = [
         args: ["d", "2s", "test.png"],
         infoLink: "/delete",
         description: `<span class="text-sm text-brand-offWhite">
-    Make a temporary file, and delete it after 2 seconds
-  </span>`,
+          Make a temporary file, and delete it after 2 seconds.
+        </span>`,
+        demo: {
+          terminal: "https://cdn.arinji.com/u/rDPGlx.mp4",
+          explorer: "https://cdn.arinji.com/u/z5vHCo.mp4",
+        },
       },
     ],
   },
