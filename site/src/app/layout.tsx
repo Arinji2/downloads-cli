4e0b0aa5d65c4ba3b81ccae40e62ba8b669144ce
Arinji2 <arinjaydhar205@gmail.com>
2025-02-09 22:38:16 +0530

    Finish Meta

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -18,6 +18,53 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://cdn.arinji.com/u/eqdgi9.png"
+          sizes="48×48"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#1D6704" />
+
+        <link rel="canonical" href="https://dos.arinji.com/" />
+        <meta
+          name="description"
+          content="Downloads on Steroids (DOS) is a tool that makes downloading files from the web easy and fast. It works using just filenames, making it cross platform, simple and easy to use. Delete, Move and Link files with ease, only on DOS"
+        />
+
+        <meta
+          name="keywords"
+          content="downloads, termina, file explorer, linux, windows"
+        />
+        <meta name="author" content="Arinji" />
+        {/* Open Graph / Facebook */}
+        <meta property="og:title" content="Downloads on Steroids" />
+        <meta
+          property="og:description"
+          content="Downloads on Steroids (DOS) is a tool that makes downloading files from the web easy and fast. It works using just filenames, making it cross platform, simple and easy to use. Delete, Move and Link files with ease, only on DOS"
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.arinji.com/u/2LEQrt.png"
+        />
+        <meta property="og:url" content="https://dos.arinji.com/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="News Nest" />
+        <meta
+          name="twitter:description"
+          content="Downloads on Steroids (DOS) is a tool that makes downloading files from the web easy and fast. It works using just filenames, making it cross platform, simple and easy to use. Delete, Move and Link files with ease, only on DOS"
+        />
+        <meta
+          name="twitter:image"
+          content="https://cdn.arinji.com/u/2LEQrt.png"
+        />
+        <title>Downloads on Steroids</title>
+      </head>
       <body
         className={`${jetBrainsMono.className} bg-brand-background py-20 antialiased w-full h-full flex flex-col items-center justify-center`}
       >
